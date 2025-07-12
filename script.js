2c9e04224b4f7b19ad1d993c57481a9aad7fb6f8
Petersimo00237 <lecreateur2006@gmail.com>
2025-07-12 23:04:21 +0100

    ajout
    ajout du site vitrine wscorporation

---
+++ script.js
@@ -0,0 +1,5 @@
+ document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  document.getElementById("form-message").textContent = "Merci pour votre message ! Nous vous répondrons bientôt.";
+  this.reset();
+  });
